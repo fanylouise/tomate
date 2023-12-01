f9fb22f4862ffea24cd7161c22c72fca47b96ebf
fanylouise <gandra.fany@gmail.com>
2023-12-01 16:38:57 -0300

    Working o/

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,20 +1,41 @@
-const clock = document.querySelector('.clock')
 let toggleClock = document.querySelector('.toggleClock')
-let minus = document.querySelector('.minus')
-let plus = document.querySelector('.plus')
 let notification = document.querySelector('.notification')
-
-let min = document.querySelector('.minutes')
+let url = document.querySelector('.url')
+//miliseconds
+let secondMili = 1000;
+let totalSeconds = 60;
+let minuteMili = 60000;
+let hourMili = 1440000;
+//variables
 let sec = document.querySelector('.seconds')
-let hour = document.querySelector('.hour')
+let min = document.querySelector('.minutes')
+let iframe= document.querySelector('.iframeDiv')
 
-let sixty = 60;
 
-let pomodoro = () => {
-     setInterval(() => {
-    if(sixty != -1){
-       sec.value = sixty--
-    }else{sixty = 59}
-  } , 10)
+// const musicLink = ()=>{
+//   iframe.innerHTML = `<iframe src="https://www.youtube.com/embed/${url.value.slice(32)}?autoplay=1&mute=0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+
+// }
+
+
+let pomodoro = ()=>{
+  sec.value=59;
+  min.value-=1 ;
+  setInterval(()=>{
+    
+   if(sec.value>0){
+    console.log(sec.value-=1) 
+   }
+ },1000)
+
+setInterval(()=>{
+  if(min.value > 0){
+    console.log(min.value-=1)
+    sec.value = 59
+  }
+},60000) 
+
 }
-toggleClock.addEventListener('click', pomodoro)
+
+  
+toggleClock.addEventListener('click',pomodoro )
